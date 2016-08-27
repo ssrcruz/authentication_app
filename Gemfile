14b4268bb76f82c27e13e4a84a115123508e01f9
Ruben <ssrcruz@gmail.com>
2016-08-27 17:28:23 -0400

    Add home static page

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   gem 'sqlite3', '1.3.11'
   gem 'byebug'
-  gem 'web-console', '~> 2.0'
+  gem 'web-console', '2.0.0.beta3'
   gem 'spring'
 end
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,6 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Authentication"
   end
-
-  test "should get help" do
-    get :help
-    assert_response :success
-  end
-
 end
