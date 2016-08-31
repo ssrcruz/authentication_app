e9d94190a80ebc71e7a986c11b28d793071b7a64
Ruben <ssrcruz@gmail.com>
2016-08-31 15:08:52 -0400

    Add to the layout test and include ApplicationHelper

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,5 +6,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
+    get signup_path
+    assert_template 'users/new'
+    assert_select 'title', full_title('Sign up')
   end
 end
